c22de07b9798b82d5bc17196112e9429a2b50aa5
moon_walker <jiotsajunior11@icloud.com>
2023-04-06 12:04:55 +0200

    suppression des headers sur tout les pages

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -15,12 +14,48 @@ const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Homescreen" component={HomeScreen} />
-        <Stack.Screen name="Main" component={MainPage} />
-        <Stack.Screen name="Test" component={Test} />
-        <Stack.Screen name="Live" component={LiveScreen} />
-        <Stack.Screen name="Teams" component={TeamsScreen} />
-        <Stack.Screen name="Stats" component={StatsScrenss} />
+        <Stack.Screen
+          name="Homescreen"
+          options={{
+            headerShown: false,
+          }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Main"
+          component={MainPage}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Test"
+          component={Test}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Live"
+          component={LiveScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Teams"
+          component={TeamsScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Stats"
+          component={StatsScrenss}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
