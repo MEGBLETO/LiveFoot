92588f89bb9ed71265dd42454349b884112a1099
MEGBLETO <carnelilordwiz@gmail.com>
2023-04-06 09:09:30 +0200

    temp

--- src/screens/TeamsScreen.js
+++ src/screens/TeamsScreen.js
@@ -2,8 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { PagecontainerWhite } from '../styles/HomeScreen.styles'
 import BottomNavigation from '../components/BottomNavigation'
+import { useNavigation } from '@react-navigation/native'
 
 const TeamsScreen = () => {
+  const navigation = useNavigation()
+
   return (
     <PagecontainerWhite>
     <BottomNavigation/>
